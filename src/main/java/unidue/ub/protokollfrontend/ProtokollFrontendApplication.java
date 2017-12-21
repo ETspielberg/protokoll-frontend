8196932bc834dda529742e890fdc1135e634f63e
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-12-21 09:36:20 +0100

    uglified UI

---
+++ src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
@@ -0,0 +1,12 @@
+package unidue.ub.protokollfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtokollFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtokollFrontendApplication.class, args);
+	}
+}
---
+++ src/test/java/unidue/ub/protokollfrontend/ProtokollFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package unidue.ub.protokollfrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProtokollFrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
