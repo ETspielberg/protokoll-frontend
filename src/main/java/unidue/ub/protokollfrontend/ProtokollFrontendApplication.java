dce48ff75fac556a65b8efe64c7b083dd8ddf805
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-16 14:24:38 +0100

    adjusting spring cloud release and cleaning up

--- src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
+++ src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
@@ -2,10 +2,13 @@ package unidue.ub.protokollfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class ProtokollFrontendApplication {
 
 	public static void main(String[] args) {
