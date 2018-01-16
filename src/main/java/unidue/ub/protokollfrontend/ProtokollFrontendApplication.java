6ae5acfb547f90069fd38146f0df183e540dd919
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-16 11:27:20 +0100

    changed button

--- src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
+++ src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
@@ -2,8 +2,10 @@ package unidue.ub.protokollfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProtokollFrontendApplication {
 
 	public static void main(String[] args) {
