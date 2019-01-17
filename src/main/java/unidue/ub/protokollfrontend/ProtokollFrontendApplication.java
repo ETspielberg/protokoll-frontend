985548bc70ca9f1b8ebdadcbf6317ac32755adef
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-01-17 08:05:02 +0100

    inclusion of spring security settings for free access

--- src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
+++ src/main/java/unidue/ub/protokollfrontend/ProtokollFrontendApplication.java
@@ -3,6 +3,10 @@ package unidue.ub.protokollfrontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,5 +14,18 @@ public class ProtokollFrontendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProtokollFrontendApplication.class, args);
+	}
+
+	@Configuration
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic()
+					.and()
+					.authorizeRequests().anyRequest().permitAll()
+					.and()
+					.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		}
 	}
 }
